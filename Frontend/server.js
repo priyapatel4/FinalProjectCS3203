e08396f1a04f9af8e88d1847ba9f67385af0551a
Priya Patel <priya.patel@ou.edu>
2022-03-24 15:07:20 -0500

    edits in server.js

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,13 +1,16 @@
 var express = require('express');//ensures Express server is used
 var app = express();//ensures Express server is used
 var bodyParser = require('body-parser');
-var obj;
+var PORT = process.env.PORT || 3000;
 const fs = require('fs');//sets us the fs for the parsing functions
 
 
 
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 
-
-app.listen(3000);// sends the application to the web port 3000
+app.listen(PORT, function() {
+    console.log('Server listening on ' + PORT);
+});
 console.log("server running on port 3000");// lets the user know on the console that the server is running.
