6f7d5fafc4b2aee5e1bdc8c4852554d11da65fb5
fran8486 <43221501+fran8486@users.noreply.github.com>
2022-03-24 14:47:53 -0500

    Create scripts.js

---
+++ Frontend/scripts.js
@@ -0,0 +1,36 @@
+$(function() {
+    // GET/READ
+    $('#get-tweets-button').on('click', function() {//this sets up the function in response to clicking the button to get all tweets
+        $.ajax({
+            type:'GET',
+            url: 'getinfo',// sets up the pathway to the server
+            contentType: 'application/json',
+            success: function(response) {//gets the response form the pathway if successful
+
+                //console.log(response);
+                var tbodyEl = $('tbody');
+
+                tbodyEl.html('');
+
+                let _dat = JSON.parse(response)
+                console.log(_dat)
+
+                //console.log(_dat[1])
+                _dat.forEach(function(obj) {// would output the id, time created, name of the person tweeting and the tweet. also adds an update and delete button to delete or update the tweet
+                    tbodyEl.append('\
+                        <tr>\
+                            <td class="id">' + obj.id + '</td>\
+                            <td><class="name">' + obj.user.name + '</td>\
+                            <td class="time">' + obj.created_at + '</td>\
+                            <td><class="tweet">' + obj.text + '</td>\
+                            <td>\
+                                <button class="update-button">UPDATE</button>\
+                                <button class="delete-button">DELETE</button>\
+                            </td>\
+                        </tr>\
+                    ');
+                });
+             }
+         });
+        
+    });
