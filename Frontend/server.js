eca11631430e69ed1a6401d6d9e806e8149816ba
fran8486 <Kaci.A.Fransen-1@ou.edu>
2022-03-24 14:49:37 -0500

    Updated on server.js

--- Frontend/server.js
+++ Frontend/server.js
@@ -3,3 +3,11 @@ var app = express();//ensures Express server is used
 var bodyParser = require('body-parser');
 var obj;
 const fs = require('fs');//sets us the fs for the parsing functions
+
+
+
+
+
+
+app.listen(3000);// sends the application to the web port 3000
+console.log("server running on port 3000");// lets the user know on the console that the server is running. 
