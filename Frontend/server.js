ac1c81690a30e93e148dae6de5d908913c3f5d16
fran8486 <43221501+fran8486@users.noreply.github.com>
2022-03-24 14:44:15 -0500

    Create server.js
    server for running our project

---
+++ Frontend/server.js
@@ -0,0 +1,5 @@
+var express = require('express');//ensures Express server is used
+var app = express();//ensures Express server is used
+var bodyParser = require('body-parser');
+var obj;
+const fs = require('fs');//sets us the fs for the parsing functions
