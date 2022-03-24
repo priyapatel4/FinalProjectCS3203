302824c90237193e2cfc5e8dc1fbd67d0d0b37df
fran8486 <Kaci.A.Fransen-1@ou.edu>
2022-03-24 15:11:50 -0500

    Commented out the example code

--- Frontend/server.js
+++ Frontend/server.js
@@ -9,6 +9,8 @@ const fs = require('fs');//sets us the fs for the parsing functions
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
+//This function is the Get Function to be used to get the information when requested. (never got this working)
+app.get('/getinfo', function(req, res) {});
 
 app.listen(PORT, function() {
     console.log('Server listening on ' + PORT);
