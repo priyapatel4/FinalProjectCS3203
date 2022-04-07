8627cdfb78bcacebb1d1fc355c3084d93f83a5cf
Priya Patel <priya.patel@ou.edu>
2022-04-07 08:50:18 -0500

    linked scripts.js and made edits to it and html button

--- scripts.js
+++ scripts.js
@@ -1,38 +1,38 @@
 $(function() {
     // GET/READ
-    // $('#showButton').on('click', function() {//this sets up the function in response to clicking the button to get all tweets
-    //     $.ajax({
-    //         type:'GET',
-    //         url: 'getinfo',// sets up the pathway to the server
-    //         contentType: 'application/json',
-    //         success: function(response) {//gets the response form the pathway if successful
-    //
-    //             //console.log(response);
-    //             var tbodyEl = $('tbody');
-    //
-    //             tbodyEl.html('');
-    //
-    //             let _dat = JSON.parse(response)
-    //             console.log(_dat)
-    //
-    //             //console.log(_dat[1])
-    //             _dat.forEach(function(obj) {// would output the id, time created, name of the person tweeting and the tweet. also adds an update and delete button to delete or update the tweet
-    //                 tbodyEl.append('\
-    //                     <tr>\
-    //                         <td class="id">' + obj.id + '</td>\
-    //                         <td><class="name">' + obj.user.name + '</td>\
-    //                         <td class="time">' + obj.created_at + '</td>\
-    //                         <td><class="tweet">' + obj.text + '</td>\
-    //                         <td>\
-    //                             <button class="update-button">UPDATE</button>\
-    //                             <button class="delete-button">DELETE</button>\
-    //                         </td>\
-    //                     </tr>\
-    //                 ');
-    //             });
-    //          }
-    //      });
+     $('#get-button').on('click', function() {//this sets up the function in response to clicking the button to get all tweets
+         $.ajax({
+             type:'GET',
+             url: 'getinfo',// sets up the pathway to the server
+             contentType: 'application/json',
+             success: function(response) {//gets the response form the pathway if successful
 
-        
+                 //console.log(response);
+                  var tbodyEl = $('tbody');
+
+                 tbodyEl.html('');
+
+                 let _dat = JSON.parse(response)
+                // console.log(_dat)
+
+                 //console.log(_dat[1])
+                _dat.forEach(function(element) {// would output the id, time created, name of the person tweeting and the tweet. also adds an update and delete button to delete or update the tweet
+                    tbodyEl.append('\
+                        <tr>\
+                            <td class="name">' + element.item_name + '</td>\
+                            <td><class="description">' + element.item_description + '</td>\
+                            <td class="category">' + element.item_category + '</td>\
+                            <td><class="price">' + element.item_price + '</td>\
+                            <td>\
+                                <button class="update-button">UPDATE</button>\
+                                <button class="delete-button">DELETE</button>\
+                            </td>\
+                        </tr>\
+                    ');
+                });
+             }
+         });
+
+     });
     });
 
--- server.js
+++ server.js
@@ -6,11 +6,38 @@ const fs = require('fs');//sets us the fs for the parsing functions
 
 var menuItems = []
 
+fs.readFile('favs.json', 'utf8', function readFileCallback(err,data ){
+    if(err){
+      //  throw err;
+        // writing new file when we want to add but no json file to add to
+        // fs.writeFile(file, JSON.stringify([obj]), error => console.error(error));
+    }
+    else{
+        let menuData = JSON.parse(data)
+        menuData.forEach(function(element) {
+            menuInfo.push({name:element.item_name,description:item_description,category:element.item_category,
+                price:element.item_price });
+        });
+    }
+});
+
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 //This function is the Get Function to be used to get the information when requested. (never got this working)
-app.get('/getinfo', function(req, res) {});
+app.get('/getinfo', function(req, res) {
+    fs.readFile('menu.json', (err, data) => {
+        if (err) {
+           // throw err;
+        }
+        else
+        {
+            let menuData = JSON.parse(data);
+            res.send(menuData)
+        }
+
+    });
+});
 
 
 app.listen(PORT, function() {
