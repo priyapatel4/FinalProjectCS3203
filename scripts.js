52c5cffca35195ee125d2227b46071cd806049bd
Priya Patel <priya.patel@ou.edu>
2022-04-07 11:15:49 -0500

    set up table and linked it to server and request

--- scripts.js
+++ scripts.js
@@ -7,22 +7,37 @@ $(function() {
              contentType: 'application/json',
              success: function(response) {//gets the response form the pathway if successful
 
-                 //console.log(response);
+                 console.log(response);
                   var tbodyEl = $('tbody');
 
                  tbodyEl.html('');
 
-                 let _dat = JSON.parse(response)
-                // console.log(_dat)
+               // let _dat = JSON.parse(response)
+              //  console.log(_dat)
+              //    response.items.forEach(function(element){
+              //        tbodyEl.append('\
+              //           <tr>\
+              //           <td class="id">' + element.item_name + '</td>\
+                 //<td><class="description">' + element.item_description + '</td>\
+              //           <td><input type="text" class="name form-control" value="' + element.item_description +'"></td>\
+                 //<td class="category">' + element.item_category + '</td>\
+              //           <td><input type="text" class="name form-control" value="' + element.item_category +'"></td>\
+                 // <td><class="price">' + element.item_price + '</td>\
+              //           \<td><input type="text" class="name form-control" value="' + element.item_price +'"></td>\
+              //           <td>\
+              //           </td>\
+              //           </tr>\
+              //           ');
+              //    });
 
-                 //console.log(_dat[1])
-                _dat.forEach(function(element) {// would output the id, time created, name of the person tweeting and the tweet. also adds an update and delete button to delete or update the tweet
+                // console.log(_dat[1])
+                response.items.forEach(function(element) {// would output the id, time created, name of the person tweeting and the tweet. also adds an update and delete button to delete or update the tweet
                     tbodyEl.append('\
                         <tr>\
                             <td class="name">' + element.item_name + '</td>\
-                            <td><class="description">' + element.item_description + '</td>\
-                            <td class="category">' + element.item_category + '</td>\
-                            <td><class="price">' + element.item_price + '</td>\
+                            <td><input type="text" class="name form-control" value="' + element.item_description +'"></td>\
+                             <td><input type="text" class="name form-control" value="' + element.item_category +'"></td>\
+                            \<td><input type="text" class="name form-control" value="' + element.item_price +'"></td>\
                             <td>\
                                 <button class="update-button">UPDATE</button>\
                                 <button class="delete-button">DELETE</button>\
--- server.js
+++ server.js
@@ -4,10 +4,14 @@ var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 const fs = require('fs');//sets us the fs for the parsing functions
 
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
 var menuItems = []
 
-fs.readFile('favs.json', 'utf8', function readFileCallback(err,data ){
+fs.readFile('menu.json', 'utf8', function readFileCallback(err,data ){
     if(err){
+        console.log("not reading file in");
       //  throw err;
         // writing new file when we want to add but no json file to add to
         // fs.writeFile(file, JSON.stringify([obj]), error => console.error(error));
@@ -15,28 +19,19 @@ fs.readFile('favs.json', 'utf8', function readFileCallback(err,data ){
     else{
         let menuData = JSON.parse(data)
         menuData.forEach(function(element) {
-            menuInfo.push({name:element.item_name,description:item_description,category:element.item_category,
+            menuItems.push({name:element.item_name,description:element.item_description,category:element.item_category,
                 price:element.item_price });
         });
+        console.log(menuItems);
     }
 });
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
+
+
 
 //This function is the Get Function to be used to get the information when requested. (never got this working)
 app.get('/getinfo', function(req, res) {
-    fs.readFile('menu.json', (err, data) => {
-        if (err) {
-           // throw err;
-        }
-        else
-        {
-            let menuData = JSON.parse(data);
-            res.send(menuData)
-        }
-
-    });
+ res.send({items: menuItems});
 });
 
 
