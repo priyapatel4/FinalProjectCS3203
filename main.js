18d8a84f0b44c7e02102b63d47f9cec1ba3beb4c
Priya Patel <priya.patel@ou.edu>
2022-04-28 15:45:29 -0500

    deleted main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// function show() {
-//     document.getElementById("displayTable").style.display = "";
-// }
-
-//function add() {
-    //var table = document.getElementById("displayTable");
-    //table.innerHTML = "";
-//}
